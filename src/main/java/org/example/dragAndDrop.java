2518e5eb1d8d4bb7fba9b980a36fc69447e0b3e6
Viktar Maroz <vmarozmail@gmail.com>
2023-01-04 17:46:25 +0100

    Made some changes and refactoring

--- src/main/java/org/example/dragAndDrop.java
+++ src/main/java/org/example/dragAndDrop.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class drugAndDrop {
+public class dragAndDrop {
 
         public static void main(String[] args) throws InterruptedException {
             System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Java\\chromedriver_win32\\chromedriver.exe");
--- src/main/java/org/example/googlerunner.java
+++ src/main/java/org/example/googlerunner.java
@@ -5,20 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class googlerunner {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Java\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        /*implicitly wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        */
         driver.get("https://google.com");
-
-
-        WebElement button = driver.findElement(By.xpath("//button [@id='L2AGLb']"));
+        WebElement button = driver.findElement(By.id("L2AGLb"));
         button.click();
 
-        WebElement input = driver.findElement(By.xpath("//input [@data-ved='0ahUKEwi-0MbSz-X6AhU-AxAIHe8ZDEsQ19QECAw']"));
-        input.click();
+        //explicit wait
+        //WebElement waitchecker = new WebDriverWait(driver,Duration.ofSeconds(10))
+                //.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a [@class='gb_7 gb_8 gb_de gb_dd']")));
+       // waitchecker.click();
+
+
 
         //Actions actions = new Actions();
         /*
@@ -30,10 +38,7 @@ public class googlerunner {
                 .release()
                 .build()
                 .perform();
-
-         */
-
-
+        */
     }
 
 }
